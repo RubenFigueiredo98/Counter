37736a00991a1aacb45d65ed2821b8a5a9d01028
Ruben Figueiredo <ruben.barroso.figueiredo@gmail.com>
2022-03-21 12:49:56 +0000

    fim de projeto

--- script.js
+++ script.js
@@ -1,30 +1,51 @@
     let minutesText = document.getElementById('minutes_1');
-    
+    let minutesText2 = document.getElementById('minutes_2');
     let secondsText = document.getElementById('seconds_1');
-    
+    let secondsText2 = document.getElementById('seconds_2');
 
     //Criar um intervalo a cada segundo
     setInterval(counter, 1000);
 
     // variaveis
-    let seconds = 00;
-    
-    let minutes = 00;
+    let seconds = 0;
+    let seconds_2 = 0;
+    let minutes = 0;
+    let minutes_2 = 0;
 
 
+    //função do contador
+    function counter() { 
 
+        // Começar o contador
+        seconds++;
 
-    function counter() {    
-    // stuff you want to do every second
-    seconds++;
-    console.log(seconds)
-    if(seconds == 60) {
-    return seconds = 0; 
-   
-    }
-     changeNumbers() 
-}
+        //Logica para incrementar por 1 minutos e segundos
+        if(seconds == 10) {
+        seconds = 0;
+        seconds_2 ++  
+        };
 
-  function changeNumbers() {
+        if(seconds_2 == 6) {
+            seconds_2 = 0;
+            minutes_2 ++
+        };
+
+        if(minutes_2 == 10) {
+            minutes_2 = 0;
+            minutes ++  
+        };
+
+        if(minutes == 6) {
+            minutes = 0
+        };
+
+        changeNumbers() 
+    };
+
+        //Função para alterar texto do ecra de acordo com os segundos e minutos.
+    function changeNumbers() {
         secondsText.innerHTML = seconds;
-    }
+        secondsText2.innerHTML = seconds_2;
+        minutesText2.innerHTML = minutes_2;
+        minutesText.innerHTML = minutes;
+    };
